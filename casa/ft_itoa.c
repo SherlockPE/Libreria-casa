92c8997b5ecdd4fc952c39f18980edad9a950484
SherlockPE <fabriciolopezreyes@gmail.com>
2023-09-20 17:36:41 +0200

    itoa nwn

--- casa/ft_itoa.c
+++ casa/ft_itoa.c
@@ -6,10 +6,28 @@
 /*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:53:20 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/20 16:53:28 by fabriciolop      ###   ########.fr       */
+/*   Updated: 2023/09/20 17:36:01 by fabriciolop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 char *ft_itoa(int n)
 {
+	char *ch;
+	
+	ch = malloc(sizeof(char *));
+	*ch = n + 48;
+	
+	return (*&ch);
 }
+
+/* int main(void)
+{
+	int	number = 9;
+	printf("%s", ft_itoa(number));
+	
+	return 0;
+}
+ */
