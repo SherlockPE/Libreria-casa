a12e96a4d3635160d0d8e3d789554eec34cd2c78
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-26 11:38:00 +0200

    update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:33:16 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/09/25 18:04:29 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/26 11:36:30 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,31 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	size_t	i;
 	char	*destiny;
 	char	*source;
-	char 	temp;
+
+	destiny = (char *)dst;
+	source = (char *)src;
+	i = 0;
+	if (dst == 0 && src == 0)
+		return (dst);
+	if (dst < src)
+		return (ft_memcpy(dst, src, n));
+	while (n--)
+		destiny[n] = source[n];
+	return (dst);
+}
+
+/* int	main(void)
+{
+	char string1[] = "123456789";
+	memcpy(string1[0], string1[5], 5);
+	printf("%s\n", string1);
+} */
+/* void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*destiny;
+	char	*source;
+	char	temp;
 
 	destiny = (char *)dst;
 	source = (char *)src;
@@ -34,33 +58,4 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		destiny[0] = temp;
 	}
 	return (destiny);
-}
-
-int	main(void)
-{
-	char string1[] = "123456789";
-	// char	string2[] = "123456789098";
-
-	memcpy(string1[0], string1[5], 5);
-	printf("%s\n", string1);
-}
-
-
-// void	*ft_memmove(void *dst, const void *src, size_t n)
-// {
-// 	size_t	i;
-// 	char	*destiny;
-// 	char	*source;
-
-// 	destiny = (char *)dst;
-// 	source = (char *)src;
-// 	i = 0;
-// 	if (dst == 0 && src == 0)
-// 		return (dst);
-// 	if (dst < src)
-// 		return (ft_memcpy(dst, src, n));
-// 	while (n--)
-// 		destiny[n] = source[n];
-// 	return (dst);
-// }
-
+} */
