16b5f0931b8363d08d31075b6651dbd165344534
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-26 16:27:08 +0200

    update ft_calloc.c and ft_strlcat.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 10:40:25 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/09/25 17:22:30 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:25:30 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (count == 0 || size == 0)
-		return (NULL);
 	ptr = malloc(count * size);
-
 	if (!ptr)
 		return (0);
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
- */
+
 // int	main(void)
 // {
 // }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 18:15:27 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/09/26 16:17:41 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:26:54 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	size_src = 0;
 	size_dst = 0;
-
-	while(src[size_src])
+	while (src[size_src])
 		size_src++;
-	while(dst[size_dst])
+	while (dst[size_dst])
 		size_dst++;
-
 	i = size_dst;
 	j = 0;
-
 	if (size_dst >= dstsize || dstsize == 0)
 		return (size_src + dstsize);
 	while (src[j] && j < dstsize - size_dst - 1)
