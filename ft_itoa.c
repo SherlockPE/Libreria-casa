ac29d21b5ad63c298fddb1b54e8504f4449a5617
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-27 18:32:15 +0200

    update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,29 +6,68 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:53:20 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/25 14:14:32 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/27 17:01:19 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "libft.h"
 
-char	*ft_itoa(int n)
+/* char	*ft_itoa(int n)
 {
-	char	*ch;
+	// char	result;
+	long	number;
+	int		size;
 
-	ch = malloc(sizeof(char *));
-	*ch = n + 48;
-	return (*&ch);
-}
+	number = (long) n;
+	size = 0;
+	while (number / 10)
+		size++;
+	printf("%d", size);
+	// if (number < 0)
+	// {
+	// 	//escribir el -
+	// 	number = -number;
+	// }
+	// if (number > 9)
+	// 	ft_itoa(number / 10);
+	// result = (number % 10) + '0';
+	//devolver valor
 
-/* int	main(void)
+// char	*final;
+} */
+
+int	main(void)
 {
-	int	number;
+	int		n;
+	char	*result;
+	long	number;
+	int		size;
+	char	*ptr;
+	int		i;
 
-	number = 9;
-	printf ("%s", ft_itoa(number));
+	number = (long) n;
+	n = 974;
+
+	i = 0;
+	size = 0;
+	while (number)
+	{
+		number /= 10;
+		size++;
+	}
+	ptr = malloc(size);
+	while (size--)
+	{
+		ptr[i] += number / 10;
+	}
+	printf("%s", ptr);
+
+	// int	number;
+
+	// number = 978;
+	// // printf ("%s", ft_itoa(number));
 
 	return (0);
 }
- */
