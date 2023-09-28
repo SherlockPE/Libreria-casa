f02f1f7509a468223a7776eb4fcc62141ca2192b
SherlockPE <fabriciolopezreyes@gmail.com>
2023-09-28 16:00:33 +0200

    Primera versión de ITOA

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,63 @@
 /*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:53:20 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/28 14:36:46 by fabriciolop      ###   ########.fr       */
+/*   Updated: 2023/09/28 15:57:04 by fabriciolop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include "libft.h"
 
 char	*ft_itoa(int n)
 {
+	int		symbol;
+	int 	temp_n;
+	int		size;
+	int		mem_symbol;
+	char	*string;
+	int		position;
+
+	//Symbol negative or positivo
+	mem_symbol = 0;
+	symbol = 1;
+	if (n < 0)
+	{
+		symbol = -symbol;
+		mem_symbol = 1;
+	}
 	
+	//Number of digits of n
+	//12345
+	size = 0;
+	temp_n = n * symbol;
+	while (temp_n != 0)
+	{
+		temp_n = temp_n / 10;
+		size++;
+	}
+	
+	//Asignación de memory y control de error por si Falla la asignación de memoria:
+	string = malloc(size + mem_symbol + 1);
+	if (string == NULL)
+		return (NULL);
+	
+	//4
+	temp_n = n * symbol;
+	position = size - mem_symbol;
+	string[position] = '\0';
+	position -= 1;
+	while (0 < position)
+	{
+		if (mem_symbol == 1)
+			string[0] = '-';
+		temp_n = temp_n / 10;
+		string[position] = (temp_n % 10) + '0';
+		position--;
+	}
+	return (string);
+	// n = 12345
+	// temp_n = 1
+	//'-','1',2','3','4','5','\0'
 }
 
 int	main(void)
@@ -34,6 +80,19 @@ int	main(void)
 	printf("Dirección de memoria: %p\n", &str);
 	free(str);
 }
+	// Necesito:
+	
+	// 	1.-Comprobar si es negativo o positivo
+	// 		1a.-Tener una variable que me ayude a la asignación de memoria por
+	// 			si es o no negativo
+	// 	2.- Saber cuántos dígitos tiene la string para asignar el espacio
+	// 		correcto de memoria
+	// 	3.-Asignar la memoria
+	// 		3a.-Asignar un byte más de memoria por si es negativo
+	//		3b.- Devolver NULL por si falla
+	// 	4.-Convertir cada digito a char y pasarlo a la memoria creada
+	// 		4a.- Tengo que arregar el valor nulo?
+	// 	5.-Devolver la string o NULL si falla la reserva de memoria
 
 /* char	*ft_itoa(int n)
 {
