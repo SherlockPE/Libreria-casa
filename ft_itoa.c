168416f1581fa9202bc981cba8c47afe9a0d4d69
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-29 19:21:38 +0200

    ITOA AL FIN VAMOS PERU update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:53:20 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/29 16:26:05 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/29 19:19:45 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void    *fill(char *str, long n, int size)
+int	ft_lendigit(long number)
 {
-    if (n < 0)
-    {
-        n *= -1;
-        str[0] = '-';
-        while (size)
-        {
-            str[size--] = (n % 10) + '0';
-            n /= 10;
-        }
-    }
-    else
-    {
-        while (size)
-        {
-            str[--size] = (n % 10) + '0';
-            n /= 10;
-        }
-    }
-    return (str);
+	int	size;
+
+	size = 0;
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		size++;
+	while (number != 0)
+	{
+		number /= 10;
+		size++;
+	}
+	return (size);
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    int        temp_n;
-    int        size;
-    char    *string;
+	int		size;
+	char	*string;
+	long	number;
+
+	number = (long)n;
+	size = ft_lendigit(number);
+	string = (char *)ft_calloc(size + 1, 1);
+	if (string == 0)
+		return (0);
+	if (number < 0)
+	{
+		string[0] = '-';
+		number *= -1;
+	}
+	while (0 < size && string[--size] != '-')
+	{
+		string[size] = (number % 10) + '0';
+		number /= 10;
+	}
+	return (string);
+}
+
+/* int	main(void)
+{
+	char	*str;
+	int		n;
+
+	n = -12345;
+	str = ft_itoa(n);
+	if (str == NULL)
+		printf("Error en la asignación de memoria");
+	printf("Todo bien, todo correctol\n");
+	printf("El valor %d fue convertido y asignado a str: %s\n", n, str);
+	printf("Dirección de memoria: %p\n", &str);
+	free(str);
+} */
+/* void	*fill(char *str, long n, int size)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		str[0] = '-';
+		while (size)
+		{
+			str[size--] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	else
+	{
+		while (size)
+		{
+			str[--size] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		temp_n;
+	int		size;
+	char	*string;
 	long	n_long;
 
 	n_long = (long)n;
@@ -51,19 +106,19 @@ char    *ft_itoa(int n)
 		string[0] = '0';
 		return (string);
 	}
-    size = 0;
-    temp_n = n_long;
-    while (temp_n != 0 && ++size)
-        temp_n /= 10;
-    if (n_long < 0)
-        string = (char *)calloc(size + 2, 1);
-    else
-        string = (char *)calloc(size + 1, 1);
-    if (!string)
-        return (0);
-    fill(string, n_long, size);
-    return (string);
-}
+	size = 0;
+	temp_n = n_long;
+	while (temp_n != 0 && ++size)
+		temp_n /= 10;
+	if (n_long < 0)
+		string = (char *)calloc(size + 2, 1);
+	else
+		string = (char *)calloc(size + 1, 1);
+	if (!string)
+		return (0);
+	fill(string, n_long, size);
+	return (string);
+} */
 
 /* int	main(void)
 {
