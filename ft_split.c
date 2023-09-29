30ea4501a3458cc6efb71bde4c76eeb798fe93be
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-29 20:11:10 +0200

    Avance split update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,28 +6,44 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:52:46 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/25 13:26:12 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/29 20:11:10 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-/* 
+
+int    n_words(char *s, int c)
+{
+    int    i;
+    int    cant_w;
+
+    i = 0;
+    cant_w = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+          i++;
+        while(s[i] != c && s[i])
+        {
+            i++;
+            if (s[i] == c)
+                cant_w ++;
+        }
+    }
+    return (cant_w);
+}
+
 char **ft_split(char const *s, char c)
 {
-	char*	string;
-	char**	array;
+    char*    string;
+    char**    array;
+    int        cant_palabras;
 
-	string = (char *) s;
-	while (*string)
-	{
-		printf("'%c', '%c'\n", *string, c);
-		string++;
-	}
-	
+    cant_palabras = n_words(s, c);
 }
 
 int main()
 {
-	ft_split("Hola mucho gusto mi nombre es Hall", ' ');
-} */
+    ft_split(",,h,ola,,,,q,ue,ta,l", ',');
+}
