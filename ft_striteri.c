03784b29321929ef92da5e2adc2f09ca8f56090b
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-09-30 13:50:02 +0200

    STRITERI update ft_striteri.c and libft.pdf

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,21 +6,33 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:54:29 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/09/25 13:26:18 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/09/30 13:44:05 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "libft.h"
+#include <unistd.h>
 
-/* void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+/* void	prueba(unsigned int n, char *c)
 {
-	while (*s);
-		f(*s);
+	(void) n;
+	printf("String -->%s\nPosition --> %d\n\n", c, n);
+} */
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
-
+/* 
 int	main()
 {
-	ft_striteri("holaaaa QUE TAL", ft_toupper());
+	char	string[] = "holaaaa QUE TAL";
+	ft_striteri(string, &prueba);
 } */
