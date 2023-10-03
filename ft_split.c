2bdf7ba877b9fbe48aaf848748e86a22eccca639
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-10-03 12:09:16 +0200

    Parte 2 TERMINADA update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:52:46 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/10/03 11:54:23 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/03 12:07:33 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,32 +36,42 @@ int n_words(char *s, int c)
     return (cant_w);
 }
 
-char    **ft_split(char const *s, char c)
+char **fill(char *s, char c, char** array)
 {
-    int        cant_words;
-    int        i;
+	int        i;
     int        start;
-    char       **array;
-
-    cant_words = n_words((char *)s, c);
-    array = (char **)ft_calloc((cant_words + 1) * 8, 1);
-    if (!array)
-        return (0);
+	int			memory;
+	
     i = 0;
+	memory = 0;
     while (s[i])
     {
-        while (s[i++] == c)
+        while (s[i] == c)
+			i++;
         start = i;
         while (s[i] != c && s[i])
         {
             i++;
             if (s[i] == c || s[i] == 0)
             {
-                *array = ft_substr(s, start, i - start);
-                *array++;
+                array[memory] = ft_substr(s, start, i - start);
+                memory++;
             }
         }
     }
+	return (array);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    int        cant_words;
+    char       **array;
+
+    cant_words = n_words((char *)s, c);
+    array = (char **)ft_calloc((cant_words + 1) * 8, 1);
+    if (!array)
+        return (0);
+	array = fill((char *)s, c, array);
 	return (array);
 }
 /* 
