8029845d850e46dd3594d811bc1afdf8cbc462be
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-10-03 09:52:11 +0200

    update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,17 +6,18 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:00:46 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/02 20:01:39 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/03 09:52:06 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* void ft_lstiter(t_list *lst, void (*f)(void *))
+/* void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	while (lst->next)
+	while (lst && lst->next)
 	{
-		lst->content = f(lst->content);
+		lst->content = &f(lst->content);
 		lst = lst->next;
 	}
-} */
+}
+ */
