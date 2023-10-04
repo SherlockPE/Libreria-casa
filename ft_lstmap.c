d5ac0a1edc81337cda802be39ff8bf5233006d5d
SherlockPE <fabriciolopezreyes@gmail.com>
2023-10-05 00:33:32 +0200

    update 3 files and delete 2 files

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,10 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:35:00 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/03 14:35:02 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/05 00:33:15 by fabriciolop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    //nueva lista a retornar
+    t_list *resultado;
+    
+    //temporal por si falla la memoria
+    t_list *temporal;
+
+    if(!lst || !f)
+        return (0);
+
+    while (lst)
+    {
+        temporal = ft_lstnew(f(lst->content));
+        if (!temporal)
+        {
+            ft_lstclear(resultado, del);
+            return (0);
+        }
+        ft_lstadd_back(resultado, temporal);
+        temporal = temporal->next;
+        lst = lst->next;
+    }
+    return (resultado);
+}
